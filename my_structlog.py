aab8b513de323249c1824ee88c28d229c4c01882
nfernandez <nfernandez@ext.bancoripley.com>
2022-09-01 16:53:01 -0300

    :art: Run linters: all files

--- my_structlog.py
+++ my_structlog.py
@@ -1,13 +1,10 @@
 import logging
+from inspect import getframeinfo, stack
 from types import FrameType
 from typing import cast
 
 import structlog
-
 from structlog.dev import ConsoleRenderer
-
-from inspect import getframeinfo, stack
-
 
 my_structlog = structlog
 
@@ -31,7 +28,7 @@ structlog.configure(
         structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
         structlog.processors.StackInfoRenderer(),
         FileNameRenderer(stack_depth=4),
-        structlog.dev.ConsoleRenderer(level_styles=ConsoleRenderer.get_default_level_styles())
+        structlog.dev.ConsoleRenderer(level_styles=ConsoleRenderer.get_default_level_styles()),
     ],
     logger_factory=structlog.stdlib.LoggerFactory(),
     cache_logger_on_first_use=True,
